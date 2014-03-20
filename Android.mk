35c2d5efd0ea0c2a48c37b24fe30fbdba6288387
Sam Judd <judds@google.com>
2014-03-20 12:00:25 -0700

    Add Android.mk file
    Change-Id: I3fba0f9addfb2591dd37be5af15ad6101ca94c3a

---
+++ Android.mk
@@ -0,0 +1,20 @@
+LOCAL_PATH := $(my-dir)
+include $(CLEAR_VARS)
+
+LOCAL_SRC_FILES := $(call all-java-files-under, library/src)
+LOCAL_RESOURCE_DIR := $(LOCAL_PATH)/library/res
+LOCAL_MANIFEST_FILE := library/AndroidManifest.xml
+
+LOCAL_STATIC_JAVA_LIBRARIES := volley
+LOCAL_STATIC_JAVA_LIBRARIES += libDiskLruCacheGlide
+
+LOCAL_MODULE := glide
+LOCAL_MODULE_TAGS := optional
+LOCAL_SDK_VERSION := 19
+
+include $(BUILD_STATIC_JAVA_LIBRARY)
+
+include $(CLEAR_VARS)
+LOCAL_PREBUILT_STATIC_JAVA_LIBRARIES := \
+  libDiskLruCacheGlide:library/libs/disklrucache-2.0.2.jar
+include $(BUILD_MULTI_PREBUILT)

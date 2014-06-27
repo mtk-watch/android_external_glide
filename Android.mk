8cdac705a0f490736d71e5cf355ae41571989d2b
Sam Judd <judds@google.com>
2014-06-26 17:10:15 -0700

    fix Android.mk
    Change-Id: I2a0a7b6430521cea9a4a0a8516879b45404cfa70

--- Android.mk
+++ Android.mk
@@ -2,10 +2,12 @@ LOCAL_PATH := $(my-dir)
 include $(CLEAR_VARS)
 
 LOCAL_SRC_FILES := $(call all-java-files-under, library/src)
+LOCAL_SRC_FILES += $(call all-java-files-under, third_party/gif_decoder/src)
 LOCAL_MANIFEST_FILE := library/AndroidManifest.xml
 
 LOCAL_STATIC_JAVA_LIBRARIES := volley
 LOCAL_STATIC_JAVA_LIBRARIES += libDiskLruCacheGlide
+LOCAL_STATIC_JAVA_LIBRARIES += android-support-v4
 
 LOCAL_MODULE := glide
 LOCAL_MODULE_TAGS := optional
@@ -15,5 +17,5 @@ include $(BUILD_STATIC_JAVA_LIBRARY)
 
 include $(CLEAR_VARS)
 LOCAL_PREBUILT_STATIC_JAVA_LIBRARIES := \
-  libDiskLruCacheGlide:library/libs/disklrucache-2.0.2.jar
+  libDiskLruCacheGlide:third_party/disklrucache/disklrucache-2.0.2.jar
 include $(BUILD_MULTI_PREBUILT)
